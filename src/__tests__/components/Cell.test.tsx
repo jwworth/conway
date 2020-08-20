a967a380d411d40b5843f1916d6d55860b300447
Jake Worth <jake@jakeworth.com>
2020-08-20 18:56:15 -0400

    [ts-migrate][.] Run TS Migrate
    Co-authored-by: ts-migrate <>

--- src/__tests__/components/Cell.test.tsx
+++ src/__tests__/components/Cell.test.tsx
@@ -6,6 +6,7 @@ import Cell from '../../components/Cell';
 it('renders without crashing', () => {
   const tr = document.createElement('tr');
   ReactDOM.render(
+    // @ts-expect-error ts-migrate(2741) FIXME: Property 'colorOfLife' is missing in type '{ cellI... Remove this comment to see the full error message
     <Cell
       cellIndex={0}
       cellValue={0}
